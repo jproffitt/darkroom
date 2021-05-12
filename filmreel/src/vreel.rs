d6d91c36b2f9761b6b6099a3a904753c3eb0dd1c
Mikhail Katychev <mkatych@gmail.com>
2021-05-12 18:42:07 -0500

    added path tests

--- filmreel/src/vreel.rs
+++ filmreel/src/vreel.rs
@@ -26,14 +26,25 @@ enum VirtualFrame<'a> {
     Frame(Frame<'a>),
 }
 
+impl<'a> From<PathBuf> for VirtualFrame<'a> {
+    fn from(p: PathBuf) -> Self {
+        Self::File(p)
+    }
+}
+
+impl<'a> From<Frame<'a>> for VirtualFrame<'a> {
+    fn from(f: Frame<'a>) -> Self {
+        Self::Frame(f)
+    }
+}
+
 #[macro_export]
 macro_rules! frames {
     ({$( $key: expr => $val: expr ),*}) => {{
-        use $crate::frame::Frame;
         use ::std::collections::BTreeMap;
 
         let mut map: BTreeMap<&str, VirtualFrame> = BTreeMap::new();
-        $(map.insert($key, VirtualFrame::Frame($val));)*
+        $(map.insert($key, VirtualFrame::from($val));)*
         map
     }}
 }
@@ -41,8 +52,8 @@ macro_rules! frames {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::test_ser_de;
-    use rstest::*;
+    use crate::{register, test_ser_de};
+
     const VREEL_JSON: &str = r#"
 {
   "name": "reel_name",
@@ -78,8 +89,28 @@ mod tests {
         VirtualReel {
             name:   "reel_name",
             frames: frames!({"1" => Frame::new(SIMPLE_FRAME_JSON).unwrap()}),
-            cut:    VirtualCut::Register(register!({"KEY" => "value})),
+            cut:    VirtualCut::Register(register!({"KEY" => "value"})),
         },
         VREEL_JSON
+    );
+
+    const PATH_VREEL_JSON: &str = r#"
+{
+  "name": "reel_name",
+  "frames": {
+    "1": "other_reel.01s.name.fr.json"
+  },
+  "cut": ["other_reel.cut.json"]
+}
+    "#;
+
+    test_ser_de!(
+        pathbuf_vframe,
+        VirtualReel {
+            name:   "reel_name",
+            frames: frames!({"1" => PathBuf::from("other_reel.01s.name.fr.json")}),
+            cut:    VirtualCut::MergeCuts(vec!["other_reel.cut.json"]),
+        },
+        PATH_VREEL_JSON
     );
 }
